0a59f6a7dd2079d5d3a347301328a3bb4ea11e09
Eduard <122577043+ink-pwd@users.noreply.github.com>
2026-05-14 23:53:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,17 +7,17 @@ echo "=== Xray CFG SETUP START ==="
 echo "[1/7] Making scripts executable..."
 chmod +x *.sh
 
-echo "[2/7] Installing dependencies..."
+echo "[2/7] Create Xray environment..."
+./create-env.sh
+
+echo "[3/7] Installing dependencies..."
 ./install-deps.sh
 
-echo "[3/7] Applying server security hardening..."
+echo "[4/7] Applying server security hardening..."
 ./secure-server.sh
 
-echo "[4/7] Installing Xray..."
+echo "[5/7] Installing Xray..."
 ./install-xray.sh
-
-echo "[5/7] Create Xray environment..."
-./create-env.sh
 
 echo "[6/7] Configuring Xray Reality..."
 ./config-reality.sh
